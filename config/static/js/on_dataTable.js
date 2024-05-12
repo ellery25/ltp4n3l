69e26ce94f6d3439ee31465dbd94605054658b6c
Ellery Ricaurte <elleryrica12@gmail.com>
2024-05-12 17:42:25 -0500

    Changing WebSocket

--- config/static/js/on_dataTable.js
+++ config/static/js/on_dataTable.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://127.0.0.1:5000"); // Asegúrate de reemplazar 'http://localhost:5000' con la URL de tu servidor
+const socket = io.connect("GohanSaiyan.pythonanywhere.com"); // Asegúrate de reemplazar 'http://localhost:5000' con la URL de tu servidor
 
 socket.on("new_dataTable", function (object) {
   // Este bloque de código se ejecutará automáticamente cada vez que el servidor emita un evento 'new_dataTable'
